2b377670a7e136c616a4bfc792be974d1fa54d9c
Siddharth Tawde <tawdesiddharth1950@gmail.com>
2026-03-02 12:40:56 +0530

    feat: add CORS support and improve root endpoint response in cart service

--- Cart/src/app.js
+++ Cart/src/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cartRoutes = require("../src/routes/cart.routes");
-
+const cors = require('cors');
 const app = express();
+
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/",(req,res)=>{
-    res.status(200).json({
-        message:"Cart service is running"
-    })
-})
+
+app.use(
+  cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3002",
+        "https://product-service-58x4.onrender.com/"
+        ],
+        accessControlAllowCredentials: true,  
+        credentials: true,
+  }),
+);
 app.use("/api/cart", cartRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Cart service is running",
+  });
+});
 
 module.exports = app;
