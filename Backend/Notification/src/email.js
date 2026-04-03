e62b54bef44417503640bc6b2f4df329f111970f
Siddharth Tawde <tawdesiddharth1950@gmail.com>
2026-04-03 11:30:07 +0530

    fix: update email sending logic to prioritize DeveloperInbox over recipient

--- Backend/Notification/src/email.js
+++ Backend/Notification/src/email.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const DeveloperInbox = process.env.DEVELOPER_INBOX;
 
 // Function to send email (same as before)
 const sendEmail = async (to, subject, text, html, attachments = []) => {
   try {
     const response = await resend.emails.send({
       from: `"BakeFlow" <${process.env.EMAIL_USER}>`,
-      to,
+      to : DeveloperInbox || to,
       subject,
       text,
       html,
